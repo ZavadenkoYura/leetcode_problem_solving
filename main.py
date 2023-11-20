731dbb977f77028bd48ddaebffd7d3998c2106c5
ZavadenkoYura <yurazavadenko43@gmail.com>
2023-11-20 16:27:37 +0200

    back to a fancy looking function

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 def main():
     for i in range(10):
-        if (i+1) % 2 == 0:
-            print("Even")
-        else:
-            print("Odd")
-
-            
+        print(f"Hello world {i}")
 
         
